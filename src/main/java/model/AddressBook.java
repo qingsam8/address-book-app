8ffa99d1780a3d0e2c1aabc0068fb98b5060d649
qingsam8 <22422345+qingsam8@users.noreply.github.com>
2021-05-15 23:31:45 +0100

    overwrite added hash and equals for AddressBook and Person

--- src/main/java/model/AddressBook.java
+++ src/main/java/model/AddressBook.java
@@ -1,14 +1,34 @@
 package model;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 public class AddressBook {
-    private List<Person> people;
+    private Set<Person> people;
 
-    public AddressBook(List<Person> people) {
+    public AddressBook(Set<Person> people) {
         this.people = people;
     }
 
     public AddressBook() {
+        this.people = new HashSet<>();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AddressBook that = (AddressBook) o;
+        return Objects.equals(people, that.people);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(people);
+    }
+
+    public Set<Person> getPeople() {
+        return people;
     }
 }
--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -2,15 +2,16 @@ package model;
 
 import consts.Gender;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.Objects;
 
 public class Person {
     private String firstName;
     private String surname;
     private Gender gender;
-    private Date dob;
+    private LocalDate dob;
 
-    public Person(String firstName, String surname, Gender gender, Date dob) {
+    public Person(String firstName, String surname, Gender gender, LocalDate dob) {
         this.firstName = firstName;
         this.surname = surname;
         this.gender = gender;
@@ -29,7 +30,25 @@ public class Person {
         return gender;
     }
 
-    public Date getDob() {
+    public LocalDate getDob() {
         return dob;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(firstName, person.firstName) &&
+                Objects.equals(surname, person.surname) &&
+                gender == person.gender &&
+                Objects.equals(dob, person.dob);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, surname, gender, dob);
+    }
+
+
 }
